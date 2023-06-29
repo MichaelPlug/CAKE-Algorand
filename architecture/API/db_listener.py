87a9ccdf39a70fcd2a283a9448995b3777997a05
Michele Spina <michelespina96@gmail.com>
2023-06-29 11:02:41 +0200

    add parameters to tune

--- architecture/API/db_listener.py
+++ architecture/API/db_listener.py
@@ -1,150 +1,140 @@
-import psycopg2
-from psycopg2 import sql
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-import time
 import json
-import requests
-import codecs
+import socket
+import ssl
+import threading
+import cipher_message
+from datetime import datetime
+import random
+import sqlite3
+from hashlib import sha512
+import block_int
+from decouple import config
+import ipfshttpclient
 
-def listen_for_trigger_events():
-    conn = psycopg2.connect(
-        host="172.18.0.3",
-        port = 5432,
-        database="postgres",
-        user="root",
-        password="root"
-    )
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curs = conn.cursor()
-    
-    # Enable LISTEN/NOTIFY for the trigger events
-    curs.execute("LISTEN update_a_row_notification;")
-    curs.execute("LISTEN update_notification;")
-    print("Waiting for trigger events...")
-    # TODO: Check if some Ready rows are already in the database
-    while True:
-        conn.poll()
-        while conn.notifies:
-            notify = conn.notifies.pop(0)
-            if notify.channel == "update_notification":
-                process_insert_event(notify.payload)
-            elif notify.channel == "update_a_row_notification":
-                process_update_event(notify.payload)
-        time.sleep(10)
-    
-    curs.close()
+api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
+
+process_instance_id = config('PROCESS_INSTANCE_ID')
+
+HEADER = 64
+PORT = int(config('SDM_PORT'))
+server_cert = 'Keys/server.crt'
+server_key = 'Keys/server.key'
+client_certs = 'Keys/client.crt'
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+"""
+creation and connection of the secure channel using SSL protocol
+"""
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.verify_mode = ssl.CERT_REQUIRED
+context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+context.load_verify_locations(cafile=client_certs)
+bindsocket = socket.socket()
+bindsocket.bind(ADDR)
+bindsocket.listen(5)
+
+"""
+function triggered by the client handler. Here starts the ciphering of the message with the policy.
+"""
+
+
+def cipher(message):
+    return cipher_message.main(message[1], message[2], message[3], message[4])
+
+
+def generate_number_to_sign(reader_address):
+    # Connection to SQLite3 sdm database
+    connection = sqlite3.connect('files/sdm/sdm.db')
+    x = connection.cursor()
+
+    now = datetime.now()
+    now = int(now.strftime("%Y%m%d%H%M%S%f"))
+    random.seed(now)
+    number_to_sign = random.randint(1, 2 ** 64)
+
+    x.execute("INSERT OR IGNORE INTO handshake_numbers VALUES (?,?,?)",
+              (str(process_instance_id), reader_address, str(number_to_sign)))
+    connection.commit()
+    return number_to_sign
+
+
+def check_handshake(reader_address, signature):
+    # Connection to SQLite3 sdm database
+    connection = sqlite3.connect('files/sdm/sdm.db')
+    x = connection.cursor()
+
+    x.execute("SELECT * FROM handshake_numbers WHERE process_instance=? AND reader_address=?",
+              (str(process_instance_id), reader_address))
+    result = x.fetchall()
+    number_to_sign = result[0][2]
+    msg = str(number_to_sign).encode()
+    public_key_ipfs_link = block_int.retrieve_publicKey(reader_address)
+    getfile = api.cat(public_key_ipfs_link)
+    getfile = getfile.split(b'###')
+    public_key_n = int(getfile[1].decode('utf-8'))
+    public_key_e = int(getfile[2].decode('utf-8').rstrip('"'))
+    if getfile[0].split(b': ')[1].decode('utf-8') == reader_address:
+        hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
+        hashFromSignature = pow(int(signature), public_key_e, public_key_n)
+        if hash == hashFromSignature:
+            print("Handshake successful")
+        else: 
+            print("Handshake failed")
+        return hash == hashFromSignature
+
+
+"""
+function that handles the requests from the clients. There is only one request possible, namely the 
+ciphering of a message with a policy.
+"""
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            # print(f"[{addr}] {msg}")
+            conn.send("Msg received!".encode(FORMAT))
+            message = msg.split('§')
+            if message[0] == "Start handshake":
+                number_to_sign = generate_number_to_sign(message[1])
+                conn.send(b'Number to be signed: ' + str(number_to_sign).encode())
+            if message[0] == "Cipher this message":
+                if check_handshake(message[4], message[5]):
+                    message_id, ipfs_link, slices, tx_id = cipher(message)
+                    conn.send(b'Here is the message_id: ' + str(message_id).encode() 
+                              + b"\n" + b'Here is the ipfs_link: ' + str(ipfs_link).encode()
+                              + b"\n" + b'Here are the slices: ' + str(slices).encode()
+                              + b"\n" + b'Here is the tx_id: ' + str(tx_id).encode())
     conn.close()
 
-def process_insert_event(payload):
-    # Process the payload received from the trigger event
-    # You can implement your custom logic here
-    print("INSERT EVENT")
-    print("Received trigger event:\n", payload)
-    res = json.loads(payload)
 
-    if res["Status"] != "Ready":
-        print("Something went wrong. Initial status is not Ready")
-        return
+"""
+main function starting the server. It listens on a port and waits for a request from a client
+"""
 
-    blob = res["File"]
-    message = bytearray.fromhex(blob[2:]).decode()
-    sender = res["From"]
-    policy = res["Policy"].strip('][').split(', ')
-    id = res["ID"]
-    list_of_entries = res["Entries"].split('], [')
-    entries = [i.strip('][').split(', ') for i in list_of_entries]
 
-    #file = blob.decode('UTF-8')
+def start():
+    bindsocket.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        newsocket, fromaddr = bindsocket.accept()
+        conn = context.wrap_socket(newsocket, server_side=True)
+        thread = threading.Thread(target=handle_client, args=(conn, fromaddr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
-    process_id = res["Process_id"]
 
-    data = {
-        "message": message,
-        "entries": entries,
-        "sender": sender,
-        "policy": policy,
-        "process_id": process_id,
-        "id": id
-    }
-
-    print("Data: ", data)
-
-    #TODO: Comminicate with API
-    response = requests.post("http://localhost:8888/dataOwner/handshake", json=data)
-    #print(response)
-    conn = psycopg2.connect(
-        host="172.18.0.3",
-        port = 5432,
-        database="postgres",
-        user="root",
-        password="root"
-    )
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curs = conn.cursor()
-    
-    curs.execute('UPDATE public."CAKE-1" SET "Status" = ' + "'Submitted'" + ' WHERE "ID" ='  + str(res["ID"]))
-    conn.commit()
-    response = requests.post("http://localhost:8888/dataOwner/cipher", json=data)
-    print(response)
-    #print(response.content)
-
-    if response.status_code != 200:
-        print("Something went wrong with the API")
-        return
-    
-    print(response.text)
-
-    #TODO: Update the database
-    conn = psycopg2.connect(
-        host="172.18.0.3",
-        port = 5432,
-        database="postgres",
-        user="root",
-        password="root",
-    )
-    curs = conn.cursor()
-    curs.execute('UPDATE public."CAKE-1" SET "Status" = ' + "'Ciphered'" + ' WHERE "ID" ='  + str(res["ID"]))
-    conn.commit()
-    curs.close()
-    print("Updated the database to Ciphered")
-    
-
-def process_update_event(payload):
-    # Process the payload received from the trigger event
-    # You can implement your custom logic here
-    print("UPDATE EVENT")
-    print("Received trigger event:\n", payload)
-    res = json.loads(payload)
-
-    if res["Status"] == "Ready":
-        process_insert_event(payload)
-        return
-    return
-    if res["Status"] == "Submitted":
-        #TODO: Comminicate with API
-
-        conn = psycopg2.connect(
-            host="localhost",
-            port = 5432,
-            database="CAKE-1",
-            user="username",
-            password="password"
-        )
-        curs = conn.cursor()
-        curs.execute('UPDATE input SET "Status" = ' + "'Ciphered'" + ' WHERE "Process_id" ='  + str(res["Process_id"]))
-        conn.commit()
-        curs.close()
-        print("Updated the database to Ciphered")
-    
-    if res["Status"] == "Ciphered":
-        print("All processes are done.")
-        return
-
-def process_trigger_event(payload):
-    # Process the payload received from the trigger event
-    # You can implement your custom logic here
-    print("Received trigger event:\n", payload)
-    print("Type of payload: ", type(payload))
-
-# Start listening for trigger events
-listen_for_trigger_events()
+print("[STARTING] server is starting...")
+start()
